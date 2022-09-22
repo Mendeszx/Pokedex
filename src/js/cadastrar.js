1c95cd501d13ad32ed0a976da3ff52ab5465d606
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-09-22 02:50:11 -0300

    adicionando tela de cadastro

---
+++ src/js/cadastrar.js
@@ -0,0 +1,29 @@
+function cadastrarUsuario(){
+    event.preventDefault()
+    let url = "https://api-pokemon-login.herokuapp.com/api/cadastrar"
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+
+    body = {
+        "id": "",
+        "nome" : nome,
+        "email": email,
+        "senha": senha
+    }
+
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = function(){
+        if(this.response == "true"){
+            window.location.href = '/login.html'
+        }else{
+            alert("Usuário já cadastrado!")
+        }
+        
+    }
+
+}
