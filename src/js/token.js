488a7820090c73b99e25e7b81e212ac4455fee53
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-09-22 01:56:30 -0300

    adicionando tela de login

--- src/js/token.js
+++ src/js/token.js
@@ -1,6 +1,4 @@
-localStorage.getItem('token')
-
-if(localStorage.getItem('token') == null){
+if(sessionStorage.getItem('token') == null){
     alert('Você precisa estar logado para acessar esssa página')
-    window.location.href = 'https://pokedex-login.herokuapp.com/'
+    window.location.href = '/login.html'
 }
