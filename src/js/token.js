0f72bca0227690897ff9bab28ffeee946b13fe66
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-09-22 01:20:24 -0300

    ajuste no token

--- src/js/token.js
+++ src/js/token.js
@@ -1,3 +1,5 @@
+localStorage.getItem('token')
+
 if(localStorage.getItem('token') == null){
     alert('Você precisa estar logado para acessar esssa página')
     window.location.href = 'https://pokedex-login.herokuapp.com/'
